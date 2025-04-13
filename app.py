147c8e74f6181b8cdab70d221c10f5e6bb7c32f0
V L Sivasai Mani Harshith Bhattaram <bhattaramvlsivasaimaniharshith@gmail.com>
2025-04-13 17:10:47 -0400

    try using all_sports table

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+# Replace values accordingly
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:mypg@localhost/matchmatics'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Define a sample model
+class all_sports(db.Model):
+    sports_id = db.Column(db.Integer, primary_key=True)
+    sports_name = db.Column(db.String(50))
+
+@app.route('/')
+def home():
+    sports = all_sports.query.all()
+    return render_template('index.html', sports=sports)
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
